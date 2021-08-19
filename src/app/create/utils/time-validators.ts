b2cd540db76a6923e6eed97ad03e6eea0a9fb540
Yannik Inniger <yannik.inniger@ti8m.ch>
2021-08-19 14:36:12 +0200

    Fix value conversion on time input polyfill

--- src/app/create/utils/time-validators.ts
+++ src/app/create/utils/time-validators.ts
@@ -3,7 +3,10 @@ import {AbstractControl} from '@angular/forms';
 export class TimeValidators {
 	static validateTime() {
 		return (control: AbstractControl): {[key: string]: boolean} | null => {
-			const isValid = /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])?$/.test(control.value?.time);
+			// the second regex validates times with AM/PM this is only needed to validate times entered in a polyfilled time field
+			const isValid =
+				/^([0-1]?[0-9]|2[0-3]):([0-5][0-9])?$/.test(control.value?.time) ||
+				/^(1[0-2]|0?[1-9]):([0-5][0-9]) ([AaPp][Mm])$/.test(control.value?.time);
 
 			if (!isValid) {
 				return {timeInvalid: true};
