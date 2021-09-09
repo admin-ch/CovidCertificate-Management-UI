18a93d7c7a9c0b41588994bce16282e5028df966
Yannik Inniger <yannik.inniger@gmail.com>
2021-09-09 16:21:00 +0200

    Improve rapid test autocomplete (#77)

---
+++ src/app/create/utils/rapid-test-validator.ts
@@ -0,0 +1,13 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+import {ProductInfoWithToString} from 'shared/model';
+
+export class RapidTestValidator {
+	static testRequired: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+		const value = control.value;
+		if (!(value instanceof ProductInfoWithToString)) {
+			return {required: true};
+		}
+
+		return null;
+	};
+}
