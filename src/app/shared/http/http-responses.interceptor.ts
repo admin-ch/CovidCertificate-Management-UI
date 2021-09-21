5e9847fa382aaab6f3b66704ec0d98dc456e986d
Fabien Cerf <fabien.cerf@bit.admin.ch>
2021-09-21 10:40:32 +0200

    VACCINECER-1138: DGC - Date of birth validation

--- src/app/shared/http/http-responses.interceptor.ts
+++ src/app/shared/http/http-responses.interceptor.ts
@@ -7,7 +7,7 @@ import {ObIObliqueHttpErrorResponse} from '@oblique/oblique/lib/http-api-interce
 
 @Injectable()
 export class HttpResponsesInterceptor implements HttpInterceptor {
-	private readonly supportedErrorCodes: number[] = [453, 470, 480, 482, 485];
+	private readonly supportedErrorCodes: number[] = [453, 470, 480, 482, 485, 489];
 
 	constructor(
 		private readonly notificationService: ObNotificationService,
