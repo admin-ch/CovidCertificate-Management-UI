e9ba2a0557e62aa8f3f17dbca1d1c74bbabf5693
Fabian Zbinden <fabian.zbinden@bit.admin.ch>
2021-11-04 12:50:22 +0100

    VACCINECERT-1509 fixed tests

--- src/app/create/select-certificate-type/select-certificate-type.component.spec.ts
+++ src/app/create/select-certificate-type/select-certificate-type.component.spec.ts
@@ -49,7 +49,7 @@ describe('SelectCertificateTypeComponent', () => {
 
 	describe('Certificate type options', () => {
 		it('should have the correct number of options', () => {
-			expect(component.typeSelection.length).toBe(3);
+			expect(component.typeSelection.length).toBe(4);
 		});
 
 		it('should have vaccination as option', () => {
