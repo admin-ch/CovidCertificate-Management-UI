0917417aeb201ad66913569add0d0358e7be6601
Yannik Inniger <yannik.inniger@gmail.com>
2021-11-24 17:05:10 +0100

    format: clean code

--- src/app/create/components/who-checkbox/who-checkbox.component.spec.ts
+++ src/app/create/components/who-checkbox/who-checkbox.component.spec.ts
@@ -21,41 +21,41 @@ describe('DateTimePickerComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(WhoCheckboxComponent);
 		component = fixture.componentInstance;
-    component.formControl = new FormControl('', Validators.required);
+		component.formControl = new FormControl('', Validators.required);
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
 
-  describe('hasError', () => {
-    it('should show error on invalid', () => {
-      component.formControl.markAllAsTouched();
-      
-      expect(component.hasError).toBeTruthy();
+	describe('hasError', () => {
+		it('should show error on invalid', () => {
+			component.formControl.markAllAsTouched();
 
-      component.formControl.setValue(undefined)
-      expect(component.hasError).toBeTruthy();
+			expect(component.hasError).toBeTruthy();
 
-      component.formControl.setValue(null)
-      expect(component.hasError).toBeTruthy();
-    });
+			component.formControl.setValue(undefined);
+			expect(component.hasError).toBeTruthy();
 
-    it('should not show error if untouched', () => {
-      expect(component.hasError).toBeFalsy();
+			component.formControl.setValue(null);
+			expect(component.hasError).toBeTruthy();
+		});
 
-      component.formControl.setValue(undefined)
-      expect(component.hasError).toBeFalsy();
+		it('should not show error if untouched', () => {
+			expect(component.hasError).toBeFalsy();
 
-      component.formControl.setValue(null)
-      expect(component.hasError).toBeFalsy();
-    });
+			component.formControl.setValue(undefined);
+			expect(component.hasError).toBeFalsy();
 
-    it('should not show error if valid', () => {
-      component.formControl.markAllAsTouched();
-      
-      component.formControl.setValue(true)
-      expect(component.hasError).toBeFalsy();
-    });
-  })
+			component.formControl.setValue(null);
+			expect(component.hasError).toBeFalsy();
+		});
+
+		it('should not show error if valid', () => {
+			component.formControl.markAllAsTouched();
+
+			component.formControl.setValue(true);
+			expect(component.hasError).toBeFalsy();
+		});
+	});
 });
--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -36,7 +36,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {ShippingComponent} from './shipping/shipping.component';
 import {DeliveryCodeCleanerDirective} from './shipping/delivery-code-cleaner.directive';
 import {TouristVaccineFormComponent} from './tourist-vaccine-form/tourist-vaccine-form.component';
-import { WhoCheckboxComponent } from './components/who-checkbox/who-checkbox.component';
+import {WhoCheckboxComponent} from './components/who-checkbox/who-checkbox.component';
 
 @NgModule({
 	declarations: [
