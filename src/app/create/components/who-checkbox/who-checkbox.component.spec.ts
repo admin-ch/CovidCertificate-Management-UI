3fa98cffef70104187a9e04ad1c1985d19275c49
Yannik Inniger <yannik.inniger@gmail.com>
2021-11-24 17:02:58 +0100

    Add test cases for who checkbox

--- src/app/create/components/who-checkbox/who-checkbox.component.spec.ts
+++ src/app/create/components/who-checkbox/who-checkbox.component.spec.ts
@@ -1,28 +1,61 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ObliqueTestingModule} from '@oblique/oblique';
+import {WhoCheckboxComponent} from './who-checkbox.component';
 
-import { WhoCheckboxComponent } from './who-checkbox.component';
+describe('DateTimePickerComponent', () => {
+	let component: WhoCheckboxComponent;
+	let fixture: ComponentFixture<WhoCheckboxComponent>;
 
-describe('WhoCheckboxComponent', () => {
-  let component: WhoCheckboxComponent;
-  let fixture: ComponentFixture<WhoCheckboxComponent>;
+	beforeEach(
+		waitForAsync(() => {
+			TestBed.configureTestingModule({
+				imports: [ObliqueTestingModule, ReactiveFormsModule],
+				declarations: [WhoCheckboxComponent],
+				schemas: [NO_ERRORS_SCHEMA]
+			}).compileComponents();
+		})
+	);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WhoCheckboxComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(() => {
+		fixture = TestBed.createComponent(WhoCheckboxComponent);
+		component = fixture.componentInstance;
+    component.formControl = new FormControl('', Validators.required);
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WhoCheckboxComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('hasError', () => {
+    it('should show error on invalid', () => {
+      component.formControl.markAllAsTouched();
+      
+      expect(component.hasError).toBeTruthy();
+
+      component.formControl.setValue(undefined)
+      expect(component.hasError).toBeTruthy();
+
+      component.formControl.setValue(null)
+      expect(component.hasError).toBeTruthy();
+    });
+
+    it('should not show error if untouched', () => {
+      expect(component.hasError).toBeFalsy();
+
+      component.formControl.setValue(undefined)
+      expect(component.hasError).toBeFalsy();
+
+      component.formControl.setValue(null)
+      expect(component.hasError).toBeFalsy();
+    });
+
+    it('should not show error if valid', () => {
+      component.formControl.markAllAsTouched();
+      
+      component.formControl.setValue(true)
+      expect(component.hasError).toBeFalsy();
+    });
+  })
 });
--- src/app/create/components/who-checkbox/who-checkbox.component.ts
+++ src/app/create/components/who-checkbox/who-checkbox.component.ts
@@ -1,21 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {Component, Input} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
-  selector: 'ec-who-checkbox',
-  templateUrl: './who-checkbox.component.html',
-  styleUrls: ['./who-checkbox.component.scss']
+	selector: 'ec-who-checkbox',
+	templateUrl: './who-checkbox.component.html',
+	styleUrls: ['./who-checkbox.component.scss']
 })
-export class WhoCheckboxComponent implements OnInit {
-  @Input() formControl: FormControl
+export class WhoCheckboxComponent {
+	@Input() formControl: FormControl;
 
-  get hasError(): boolean {
-    return this.formControl.errors?.required && this.formControl?.touched;
-  }
+	get hasError(): boolean {
+		return this.formControl.errors?.required && this.formControl?.touched;
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor() {}
 }
