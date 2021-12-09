dbaebb2e07aff863c053b510e3513f02975c31c7
Fabian Zbinden <fabian.zbinden@bit.admin.ch>
2021-12-09 14:44:03 +0100

    format: clean code

--- src/app/create/components/who-checkbox/who-checkbox.component.ts
+++ src/app/create/components/who-checkbox/who-checkbox.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {GenerationType} from "shared/model";
-import {TranslateService} from "@ngx-translate/core";
+import {GenerationType} from 'shared/model';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
 	selector: 'ec-who-checkbox',
@@ -10,15 +10,14 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class WhoCheckboxComponent {
 	@Input() formControl: FormControl;
-	@Input() certificateType?: GenerationType
+	@Input() certificateType?: GenerationType;
 
-	constructor(private translate: TranslateService) {
-	}
+	constructor(private translate: TranslateService) {}
 
 	get infoText(): string {
 		let text: string = this.translate.instant('certificateCreate.step-two.entitledtoissueconfirmation');
 
-		if(this.certificateType !== 'recovery') {
+		if (this.certificateType !== 'recovery') {
 			text += this.translate.instant('certificateCreate.step-two.nonissuablevaccineproductinformation');
 		}
 
