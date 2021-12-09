afedbe7505cc6768377766bb925053a1a8a14330
Fabian Zbinden <fabian.zbinden@bit.admin.ch>
2021-12-09 14:43:20 +0100

    VACCINECERT-1626 Now who-checkbox component also gets used by recovery certificate form

--- src/app/create/components/who-checkbox/who-checkbox.component.ts
+++ src/app/create/components/who-checkbox/who-checkbox.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {GenerationType} from "shared/model";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
 	selector: 'ec-who-checkbox',
@@ -8,6 +10,20 @@ import {FormControl} from '@angular/forms';
 })
 export class WhoCheckboxComponent {
 	@Input() formControl: FormControl;
+	@Input() certificateType?: GenerationType
+
+	constructor(private translate: TranslateService) {
+	}
+
+	get infoText(): string {
+		let text: string = this.translate.instant('certificateCreate.step-two.entitledtoissueconfirmation');
+
+		if(this.certificateType !== 'recovery') {
+			text += this.translate.instant('certificateCreate.step-two.nonissuablevaccineproductinformation');
+		}
+
+		return text;
+	}
 
 	get hasError(): boolean {
 		return this.formControl.errors?.required && this.formControl?.touched;
