fae6c3321fef71d4488234bbf15419b80d5b2dea
andreascafuto <andreascafuto@gmail.com>
2022-01-31 17:23:19 +0100

    VACCINCER-1967:Fixed issue with untranslated fields in summary-page

--- src/app/create/summary/summary.component.ts
+++ src/app/create/summary/summary.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {Patient, Shipping} from 'shared/model';
-import {CreationDataService} from '../utils/creation-data.service';
-import {CertificateService} from 'shared/certificate.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CertificateService } from 'shared/certificate.service';
+import { Patient, Shipping } from 'shared/model';
+import { CreationDataService } from '../utils/creation-data.service';
+import { ValueSetsService } from '../utils/value-sets.service';
 
 @Component({
 	selector: 'ec-summary',
@@ -16,6 +17,8 @@ export class SummaryComponent implements OnInit {
 	validFrom: Date;
 	validUntil: Date;
 	shipping: Shipping;
+	patientTypeOfTest: string;
+	patientVaccinationCountry: string;
 
 	private readonly RECOVERY_CERTIFICATE_VALIDITY_IN_DAYS = 364;
 	private readonly DAYS_UNTIL_VALID = 10;
@@ -23,7 +26,7 @@ export class SummaryComponent implements OnInit {
 	get birthdate(): string {
 		const patientBirthdate = this.patient?.birthdate;
 		if (patientBirthdate instanceof Date) {
-			return patientBirthdate.toLocaleDateString('de-CH', {day: '2-digit', month: '2-digit', year: 'numeric'});
+			return patientBirthdate.toLocaleDateString('de-CH', { day: '2-digit', month: '2-digit', year: 'numeric' });
 		} else {
 			return patientBirthdate;
 		}
@@ -31,8 +34,9 @@ export class SummaryComponent implements OnInit {
 
 	constructor(
 		private readonly dataService: CreationDataService,
+		private readonly valueSetsService: ValueSetsService,
 		private readonly certificateService: CertificateService
-	) {}
+	) { }
 
 	ngOnInit(): void {
 		this.dataService.patientChanged.subscribe(patient => {
@@ -51,6 +55,22 @@ export class SummaryComponent implements OnInit {
 		this.dataService.shippingChanged.subscribe(shipping => {
 			this.shipping = shipping;
 		});
+	}
+
+	getTranslatedTypeOfTest(): string {
+		return this.valueSetsService.getTypeOfTests().filter(elem => elem.code === this.patient?.test.typeOfTest.code).map(item => item.display)[0];
+	}
+
+	getTranslatedCountry(): string {
+		if (this.patient.test) {
+			return this.valueSetsService.getCountryOptions().filter(elem => elem.code === this.patient?.test.countryOfTest.code).map(item => item.display)[0];
+		}
+		if (this.patient.vaccination) {
+			return this.valueSetsService.getCountryOptions().filter(elem => elem.code === this.patient?.vaccination.countryOfVaccination.code).map(item => item.display)[0];
+		}
+		if (this.patient.recovery) {
+			return this.valueSetsService.getCountryOptions().filter(elem => elem.code === this.patient?.recovery.countryOfTest.code).map(item => item.display)[0];
+		}
 	}
 
 	goBack(): void {
