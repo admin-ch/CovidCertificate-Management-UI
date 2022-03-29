81f06d121e5cfe4b4208e47458e042e5b440c4e0
andreascafuto <andreascafuto@gmail.com>
2022-03-29 13:49:05 +0200

    format: clean code

--- src/app/create/components/retroactive-radio/retroactive-radio.component.spec.ts
+++ src/app/create/components/retroactive-radio/retroactive-radio.component.spec.ts
@@ -1,33 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {TranslateModule} from '@ngx-translate/core';
 
-
-import { RetroactiveRadioComponent } from './retroactive-radio.component';
+import {RetroactiveRadioComponent} from './retroactive-radio.component';
 
 describe('RetroactiveRadioComponent', () => {
-  let component: RetroactiveRadioComponent;
-  let fixture: ComponentFixture<RetroactiveRadioComponent>;
+	let component: RetroactiveRadioComponent;
+	let fixture: ComponentFixture<RetroactiveRadioComponent>;
 
-  beforeEach(async () => {
-	  await TestBed.configureTestingModule({
-      declarations: [RetroactiveRadioComponent],
-      imports: [
-        TranslateModule.forRoot(),
-      ],
-		  schemas: [NO_ERRORS_SCHEMA]
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [RetroactiveRadioComponent],
+			imports: [TranslateModule.forRoot()],
+			schemas: [NO_ERRORS_SCHEMA]
+		}).compileComponents();
+	});
 
-	  })
-      .compileComponents();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(RetroactiveRadioComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RetroactiveRadioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
