94d0391c4c0c3e38f51537b6552ea5bfb9ac00db
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-05-27 09:38:04 +0200

    rearranged some stuff to be more generic

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -4,7 +4,7 @@ import {SharedModule} from 'shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {AuthGuardService} from '../auth/auth-guard.service';
 import {MatIconModule} from '@angular/material/icon';
-import {ObButtonModule, ObCollapseModule, ObIconModule, ObPopoverModule} from '@oblique/oblique';
+import {ObAlertModule, ObButtonModule, ObCollapseModule, ObIconModule, ObPopoverModule} from '@oblique/oblique';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -13,6 +13,9 @@ import {MatStepperModule} from '@angular/material/stepper';
 import {SelectReportTypeComponent} from './select-report-type/select-report-type.component';
 import {MatRadioModule} from "@angular/material/radio";
 import {MatCardModule} from "@angular/material/card";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatInputModule} from "@angular/material/input";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 @NgModule({
 	declarations: [ReportComponent, SelectReportTypeComponent],
@@ -29,7 +32,12 @@ import {MatCardModule} from "@angular/material/card";
 		MatRadioModule,
 		ObCollapseModule,
 		ObPopoverModule,
-		MatCardModule
+		MatCardModule,
+		MatChipsModule,
+		MatInputModule,
+		MatProgressBarModule,
+		ObAlertModule
 	]
 })
-export class ReportModule {}
+export class ReportModule {
+}
