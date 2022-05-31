ef60497646bd6b4d3df7e08ec7194976c174ebe6
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-05-31 09:09:32 +0200

    fixed uvci validator

--- src/app/create/utils/uvci-validator.ts
+++ src/app/create/utils/uvci-validator.ts
@@ -1,7 +1,7 @@
 import {AbstractControl, ValidationErrors} from '@angular/forms';
 
 export const uvciValidator = (control: AbstractControl): ValidationErrors | null => {
-	if (!(control.value as string)?.match(/^urn:uvci:01:CH:\d{24}$/g)) {
+	if (!(control.value as string)?.match(/^urn:uvci:01:CH:[a-zA-Z0-9]{24}$/g)) {
 		return {
 			format: true
 		}
