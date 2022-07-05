f7c0b743e5b8752f45c9ff329f43f7f3c00619e1
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-07-05 10:48:27 +0200

    extracted report fields date from, date to, data room selection and certificate type selection to separate components

---
+++ src/app/report/report-parameter/_shared/data-room-selection-fieldset/data-room-selection-fieldset.component.spec.ts
@@ -0,0 +1,73 @@
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+
+import {DataRoomSelectionFieldsetComponent} from './data-room-selection-fieldset.component';
+import {AuthService} from "../../../../auth/auth.service";
+import {DataRoomCode} from "shared/model";
+import {Subject} from "rxjs";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {ObliqueTestingModule} from "@oblique/oblique";
+import {TranslateModule} from "@ngx-translate/core";
+import {FormControl} from "@angular/forms";
+import {ReportService} from "../../../report.service";
+
+describe('DataRoomSelectionComponent', () => {
+	let component: DataRoomSelectionFieldsetComponent;
+	let fixture: ComponentFixture<DataRoomSelectionFieldsetComponent>;
+	let authService: AuthService
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ObliqueTestingModule, TranslateModule],
+			declarations: [DataRoomSelectionFieldsetComponent],
+			providers: [
+				{
+					provide: AuthService,
+					useValue: {
+						authorizedDataRooms$: new Subject<DataRoomCode[]>()
+					}
+				},
+				{
+					provide: ReportService,
+					useValue: {
+						reset$: new Subject<void>()
+					}
+				},
+			],
+			schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
+		})
+			.compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(DataRoomSelectionFieldsetComponent);
+		component = fixture.componentInstance;
+
+		component.dataRoomFormControl = new FormControl('')
+		fixture.detectChanges();
+		authService = TestBed.inject(AuthService)
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	describe('authorizedDataRooms$', () => {
+
+		it('should set canton automatically if there is only one available to the user', fakeAsync(() => {
+			component.dataRoomFormControl.setValue(null)
+			// @ts-ignore
+			authService.authorizedDataRooms$.next([DataRoomCode.AG])
+			tick()
+
+			expect(component.dataRoomFormControl.value).toEqual(DataRoomCode.AG)
+		}));
+		it('should not set canton automatically if there are multiple cantons available to the user', fakeAsync(() => {
+			component.dataRoomFormControl.setValue(null)
+			// @ts-ignore
+			authService.authorizedDataRooms$.next([DataRoomCode.AG, DataRoomCode.BE])
+			tick()
+
+			expect(component.dataRoomFormControl.value).toBe(null)
+		}));
+	});
+});
---
+++ src/app/report/report-parameter/_shared/data-room-selection-fieldset/data-room-selection-fieldset.component.ts
@@ -0,0 +1,47 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {take, tap} from "rxjs/operators";
+import {AuthService} from "../../../../auth/auth.service";
+import {Observable, Subscription} from "rxjs";
+import {DataRoomCode} from "shared/model";
+import {FormControl} from "@angular/forms";
+import {ReportService} from "../../../report.service";
+
+@Component({
+	selector: 'ec-data-room-selection-fieldset',
+	templateUrl: './data-room-selection-fieldset.component.html',
+	styleUrls: ['./data-room-selection-fieldset.component.scss']
+})
+export class DataRoomSelectionFieldsetComponent implements OnInit, OnDestroy {
+
+	@Input()
+	dataRoomFormControl: FormControl
+
+	authorizedDataRooms$: Observable<DataRoomCode[]>
+
+	subscription: Subscription
+
+	constructor(private readonly auth: AuthService,
+				private readonly reportService: ReportService) {
+	}
+
+	ngOnInit(): void {
+		this.subscription = this.reportService.reset$.subscribe(() => this.resetControl())
+		this.authorizedDataRooms$ = this.auth.authorizedDataRooms$.pipe(
+			tap(dataRooms => {
+				// Pre-select the canton if there is only one available.
+				if (dataRooms?.length === 1) {
+					this.dataRoomFormControl.setValue(dataRooms[0])
+				}
+			})
+		)
+	}
+
+	ngOnDestroy() {
+		this.subscription?.unsubscribe()
+	}
+
+	resetControl() {
+		// The `tap()` function defined in `ngOnInit()` takes care of populating the `canton` formControl.
+		this.authorizedDataRooms$.pipe(take(1)).subscribe()
+	}
+}
