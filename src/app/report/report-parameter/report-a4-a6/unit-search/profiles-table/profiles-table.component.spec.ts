7296bca4644c1612ed86298db32a386814ad2301
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-07-19 12:00:19 +0200

    user profiles table is now shoing on each tree, not only the last

---
+++ src/app/report/report-parameter/report-a4-a6/unit-search/profiles-table/profiles-table.component.spec.ts
@@ -0,0 +1,45 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ProfilesTableComponent} from './profiles-table.component';
+import {ObliqueTestingModule} from "@oblique/oblique";
+import {TranslateModule} from "@ngx-translate/core";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatTableModule} from "@angular/material/table";
+import {SelectedProfilesService} from "../../selected-profiles.service";
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from "@angular/core";
+import {UnitTree} from "../unit-search.component";
+import {NestedTreeControl} from "@angular/cdk/tree";
+
+describe('ProfilesTableComponent', () => {
+	let component: ProfilesTableComponent;
+	let fixture: ComponentFixture<ProfilesTableComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ObliqueTestingModule, HttpClientTestingModule, TranslateModule.forRoot(), MatTableModule],
+			declarations: [ProfilesTableComponent],
+			providers: [
+				{provide: 'REPORT_HOST', useValue: 'REPORT_HOST'},
+				{provide: SelectedProfilesService, useValue: new SelectedProfilesService()},
+			],
+			schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+		})
+			.compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ProfilesTableComponent);
+		component = fixture.componentInstance;
+
+		component.authority = 'BUV'
+		component.node = {} as any
+		component.treeControl = new NestedTreeControl<UnitTree>(_ => [])
+
+
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
