b0da03ecfeea41651eaa1ca5ddc1423c87ae28c4
Matthieu Riolo <matthieu.riolo@bit.admin.ch>
2022-08-30 19:19:09 +0200

    move FieldWrapperComponent into _shared module

--- src/app/report/report.module.ts
+++ src/app/report/report.module.ts
@@ -41,7 +41,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {FormsModule} from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
 import {SelectedProfilesComponent} from './report-parameter/_shared/selected-profiles/selected-profiles.component';
-import {FieldWrapperComponent} from './report-parameter/report-a4-a6/field-wrapper/field-wrapper.component';
+import {FieldWrapperComponent} from './report-parameter/_shared/field-wrapper/field-wrapper.component';
 import {IssuerSearchComponent} from './report-parameter/_shared/issuer-search/issuer-search.component';
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatSortModule} from "@angular/material/sort";
