900ffee739883efb9608bf7c5c404359bab186b2
Nino Di Natale <nino.dinatale@bit.admin.ch>
2022-11-11 11:42:56 +0100

    updated oblique from v8 to v9

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import {OB_PROJECT_INFO} from '@oblique/oblique';
+import packageInfo from '../package.json';
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
@@ -7,8 +9,7 @@ if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowserDynamic([
-	{provide: 'HOST', useValue: environment.host},
+platformBrowserDynamic([{provide: 'HOST', useValue: environment.host},
 	{provide: 'NOTIFICATION_HOST', useValue: environment.notificationHost},
 	{
 		provide: 'IS_NOTIFICATION_SERVICE_ENABLED',
@@ -17,7 +18,8 @@ platformBrowserDynamic([
 		// useFactory: () => true
 	},
 	{provide: 'REPORT_HOST', useValue: environment.reportHost},
-	{provide: 'EIAM_SELF_ADMIN', useValue: environment.eiamSelfAdmin}
+	{provide: 'EIAM_SELF_ADMIN', useValue: environment.eiamSelfAdmin},
+{provide: OB_PROJECT_INFO, useValue: {name: packageInfo.name, version: packageInfo.version, title: packageInfo.title}}
 ])
 	.bootstrapModule(AppModule)
 	.catch(err => console.error(err));
