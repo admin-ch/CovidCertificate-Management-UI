064547c70f96357d88e3663c6ad339f95355e687
Nino Di Natale <nino.dinatale@bit.admin.ch>
2023-01-12 15:02:31 +0100

    fixed URL

--- src/app/cache-reset/cache-reset.service.ts
+++ src/app/cache-reset/cache-reset.service.ts
@@ -1,15 +1,13 @@
 import {Injectable} from '@angular/core';
-import {ApiService} from "shared/api.service";
+import {ApiService} from 'shared/api.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class CacheResetService {
-
-	constructor(private readonly http: ApiService) {
-	}
+	constructor(private readonly http: ApiService) {}
 
 	resetCache() {
-		this.http.post("clear", null ).subscribe();
+		this.http.post('caches/clear', null).subscribe();
 	}
 }
