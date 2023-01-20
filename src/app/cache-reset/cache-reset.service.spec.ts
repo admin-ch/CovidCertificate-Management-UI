39baa511dd0194bbebc3adc6d12896203d0b1e26
Yanik Fenner <yanik.fenner@bit.admin.ch>
2023-01-20 17:24:50 +0100

    Fixing Git Action

--- src/app/cache-reset/cache-reset.service.spec.ts
+++ src/app/cache-reset/cache-reset.service.spec.ts
@@ -1,12 +1,26 @@
 import {TestBed} from '@angular/core/testing';
-
 import {CacheResetService} from './cache-reset.service';
+import {NotificationService} from '../notifications/notification.service';
+import {Subject} from 'rxjs';
+import {ObNotificationService} from '@oblique/oblique';
+import {ApiService} from 'shared/api.service';
 
 describe('CacheResetService', () => {
 	let service: CacheResetService;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			providers: [
+				{
+					provide: NotificationService,
+					useValue: {
+						imminentNotifications$: new Subject()
+					}
+				},
+				{provide: ObNotificationService, useValue: {}},
+				{provide: ApiService, useValue: {}}
+			]
+		});
 		service = TestBed.inject(CacheResetService);
 	});
 
